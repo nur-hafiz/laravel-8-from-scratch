bd5b9e102cc5c0a90efbf4614006a18e33d09030
unknown <mdnur.hafiz@outlook.com>
2021-06-13 15:59:43 +0800

    Eager load categories with posts

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/posts', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
