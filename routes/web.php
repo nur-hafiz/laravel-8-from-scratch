cb65166ecf9df69f2a0e6b4b1e95679f804cd415
Hafiz <mdnur.hafiz@outlook.com>
2021-06-15 20:10:06 +0800

    Change /posts route to /

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Post::find('my-first-post');
-    return view('welcome');
-});
-
-Route::get('/posts', function () {
     return view('posts', [
         'posts' => Post::latest()->get()
 
